d5abfa12ba3c56edafcab080fc77e24af9d8df6a
李志伦 <313701855@qq.com>
2019-10-14 16:50:19 +0800

    按顺序展示查询结果

--- app/src/main/java/com/lizl/mydiary/dao/DiaryDao.kt
+++ app/src/main/java/com/lizl/mydiary/dao/DiaryDao.kt
@@ -4,10 +4,9 @@ import androidx.room.Dao
 import androidx.room.Query
 import com.lizl.mydiary.bean.DiaryBean
 
-@Dao
-interface DiaryDao : BaseDao<DiaryBean>
+@Dao interface DiaryDao : BaseDao<DiaryBean>
 {
-    @Query("select * from diaries") fun getAllDiary(): MutableList<DiaryBean>
+    @Query("select * from diaries order by createTime desc") fun getAllDiary(): MutableList<DiaryBean>
 
-    @Query("select count (*) from diaries") fun getDiaryCount() : Int
+    @Query("select count (*) from diaries") fun getDiaryCount(): Int
 }
