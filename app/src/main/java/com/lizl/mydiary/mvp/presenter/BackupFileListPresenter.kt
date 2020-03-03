949011392526517214cb5fee78996674103d69aa
lizhilun <313701855@qq.com>
2020-03-03 22:00:20 +0800

    显示弹窗添加延时

--- app/src/main/java/com/lizl/mydiary/mvp/presenter/BackupFileListPresenter.kt
+++ app/src/main/java/com/lizl/mydiary/mvp/presenter/BackupFileListPresenter.kt
@@ -4,54 +4,62 @@ import com.lizl.mydiary.event.UIEvent
 import com.lizl.mydiary.mvp.contract.BackupFileListContract
 import com.lizl.mydiary.util.BackupUtil
 import com.lizl.mydiary.util.FileUtil
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import java.io.File
 
-class BackupFileListPresenter(private var view: BackupFileListContract.View?) : BackupFileListContract.Presenter
-{
+class BackupFileListPresenter(private var view: BackupFileListContract.View?) :
+    BackupFileListContract.Presenter {
 
-    override fun getBackupFileList()
-    {
+    override fun getBackupFileList() {
         GlobalScope.launch {
-            GlobalScope.launch(Dispatchers.Main) { view?.showFileFindingView() }
+            val job = GlobalScope.launch(Dispatchers.Main) {
+                delay(200)
+                view?.showFileFindingView()
+            }
             val backupFileList = BackupUtil.getBackupFileList()
+            job.cancel()
             GlobalScope.launch(Dispatchers.Main) { view?.showBackupFileList(backupFileList) }
         }
     }
 
-    override fun deleteBackupFile(file: File)
-    {
+    override fun deleteBackupFile(file: File) {
         GlobalScope.launch {
             val result = FileUtil.deleteFile(file.absolutePath)
             GlobalScope.launch(Dispatchers.Main) { if (result) view?.onBackupFileDeleted(file) }
         }
     }
 
-    override fun restoreData(file: File)
-    {
+    override fun restoreData(file: File) {
         GlobalScope.launch {
             GlobalScope.launch(Dispatchers.Main) { view?.showRestoringDataView() }
             BackupUtil.restoreData(file.absolutePath) { result, failedReason ->
-                GlobalScope.launch(Dispatchers.Main) { view?.onRestoreDataFinish(result, file, failedReason) }
+                GlobalScope.launch(Dispatchers.Main) {
+                    view?.onRestoreDataFinish(
+                        result,
+                        file,
+                        failedReason
+                    )
+                }
             }
         }
     }
 
-    override fun restoreData(file: File, password: String)
-    {
+    override fun restoreData(file: File, password: String) {
         GlobalScope.launch {
             GlobalScope.launch(Dispatchers.Main) { view?.showRestoringDataView() }
             BackupUtil.restoreData(file.absolutePath, password) { result, failedReason ->
-                GlobalScope.launch(Dispatchers.Main) { view?.onRestoreDataFinish(result, file, failedReason) }
+                GlobalScope.launch(Dispatchers.Main) {
+                    view?.onRestoreDataFinish(
+                        result,
+                        file,
+                        failedReason
+                    )
+                }
             }
         }
     }
 
-    override fun renameBackupFile(file: File, newName: String)
-    {
+    override fun renameBackupFile(file: File, newName: String) {
         GlobalScope.launch {
             FileUtil.renameFile(file, newName, true)
             delay(500)
@@ -59,8 +67,7 @@ class BackupFileListPresenter(private var view: BackupFileListContract.View?) : 
         }
     }
 
-    override fun clearBackupFiles()
-    {
+    override fun clearBackupFiles() {
         GlobalScope.launch {
             val backupFileList = BackupUtil.getBackupFileList()
             val latestFile = backupFileList.maxBy { it.lastModified() } ?: return@launch
@@ -69,13 +76,11 @@ class BackupFileListPresenter(private var view: BackupFileListContract.View?) : 
         }
     }
 
-    override fun handleUIEvent(uiEvent: UIEvent)
-    {
+    override fun handleUIEvent(uiEvent: UIEvent) {
 
     }
 
-    override fun onDestroy()
-    {
+    override fun onDestroy() {
         view = null
     }
 }
