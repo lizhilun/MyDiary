043a226f070eb79bbe789e2b2f5f28d9ab344445
李志伦 <313701855@qq.com>
2020-04-13 16:19:43 +0800

    修复loading弹窗按返回键时消失的问题

--- app/src/main/java/com/lizl/mydiary/custom/popup/PopupLoading.kt
+++ app/src/main/java/com/lizl/mydiary/custom/popup/PopupLoading.kt
@@ -3,15 +3,20 @@ package com.lizl.mydiary.custom.popup
 import android.content.Context
 import com.lizl.mydiary.R
 import com.lxj.xpopup.core.CenterPopupView
-import kotlinx.android.synthetic.main.dialog_loading.view.*
+import kotlinx.android.synthetic.main.popup_loading.view.*
 
 class PopupLoading(context: Context, private val loadingText: String) : CenterPopupView(context)
 {
-    override fun getImplLayoutId() = R.layout.dialog_loading
+    override fun getImplLayoutId() = R.layout.popup_loading
 
     override fun onCreate()
     {
         tv_text.text = loadingText
         loading_view.smoothToShow()
+
+        popupInfo?.let {
+            it.isDismissOnBackPressed = false
+            it.isDismissOnTouchOutside = false
+        }
     }
 }
--- app/src/main/java/com/lizl/mydiary/util/PopupUtil.kt
+++ app/src/main/java/com/lizl/mydiary/util/PopupUtil.kt
@@ -12,99 +12,80 @@ import com.lizl.mydiary.bean.OperationItem
 import com.lizl.mydiary.custom.popup.*
 import com.lxj.xpopup.XPopup
 import com.lxj.xpopup.core.BasePopupView
+import kotlinx.coroutines.*
 
 
 object PopupUtil
 {
-    private var popup: BasePopupView? = null
+    private var curPopup: BasePopupView? = null
     private var dialog: Dialog? = null
+    private var showPopupJob: Job? = null
 
     fun showLoadingPopup(loadingText: String)
     {
         val context = ActivityUtils.getTopActivity() ?: return
-        dismissAll()
-        popup = XPopup.Builder(ActivityUtils.getTopActivity()).asCustom(PopupLoading(context, loadingText))
-        popup?.show()
+        showPopup(XPopup.Builder(ActivityUtils.getTopActivity()).asCustom(PopupLoading(context, loadingText)))
     }
 
     fun showOperationConfirmPopup(title: String, notify: String, onConfirmBtnClickListener: () -> Unit)
     {
         val context = ActivityUtils.getTopActivity() ?: return
-        dismissAll()
-        popup = XPopup.Builder(context).asConfirm(title, notify) { onConfirmBtnClickListener.invoke() }.bindLayout(R.layout.popup_operation_confirm)
-        popup?.show()
+        showPopup(XPopup.Builder(context).asConfirm(title, notify) { onConfirmBtnClickListener.invoke() }.bindLayout(R.layout.popup_operation_confirm))
     }
 
     fun showOperationListPopup(operationList: List<OperationItem>)
     {
         val context = ActivityUtils.getTopActivity() ?: return
-        dismissAll()
-        popup = XPopup.Builder(context).asCustom(PopupOperationList(context, operationList))
-        popup?.show()
+        showPopup(XPopup.Builder(context).asCustom(PopupOperationList(context, operationList)))
     }
 
     fun showPasswordConfirmPopup(password: String, onInputFinishListener: (String) -> Unit)
     {
         val context = ActivityUtils.getTopActivity() ?: return
-        dismissAll()
-        popup = XPopup.Builder(context).asCustom(PopupPassword(context, PopupPassword.PASSWORD_OPERATION_CHECK, password, onInputFinishListener))
-        popup?.show()
+        showPopup(XPopup.Builder(context).asCustom(PopupPassword(context, PopupPassword.PASSWORD_OPERATION_CHECK, password, onInputFinishListener)))
     }
 
     fun showSetPasswordPopup(onInputFinishListener: (String) -> Unit)
     {
         val context = ActivityUtils.getTopActivity() ?: return
-        dismissAll()
-        popup = XPopup.Builder(context).asCustom(PopupPassword(context, PopupPassword.PASSWORD_OPERATION_NEW, onInputFinishListener = onInputFinishListener))
-        popup?.show()
+        showPopup(XPopup.Builder(context).asCustom(PopupPassword(context, PopupPassword.PASSWORD_OPERATION_NEW, onInputFinishListener = onInputFinishListener)))
     }
 
     fun showModifyPasswordPopup(oldPassword: String, onInputFinishListener: (String) -> Unit)
     {
         val context = ActivityUtils.getTopActivity() ?: return
-        dismissAll()
-        popup = XPopup.Builder(context).asCustom(PopupPassword(context, PopupPassword.PASSWORD_OPERATION_MODIFY, oldPassword, onInputFinishListener))
-        popup?.show()
+        showPopup(XPopup.Builder(context).asCustom(PopupPassword(context, PopupPassword.PASSWORD_OPERATION_MODIFY, oldPassword, onInputFinishListener)))
     }
 
     fun showInputPasswordPopup(onInputFinishListener: (String) -> Unit)
     {
         val context = ActivityUtils.getTopActivity() ?: return
-        dismissAll()
-        popup = XPopup.Builder(context).asCustom(PopupPassword(context, PopupPassword.PASSWORD_OPERATION_INPUT, onInputFinishListener = onInputFinishListener))
-        popup?.show()
+        showPopup(
+                XPopup.Builder(context).asCustom(PopupPassword(context, PopupPassword.PASSWORD_OPERATION_INPUT, onInputFinishListener = onInputFinishListener)))
     }
 
     fun showRadioGroupPopup(title: String, radioList: List<String>, checkedRadio: String, onSelectFinishListener: (result: String) -> Unit)
     {
         val context = ActivityUtils.getTopActivity() ?: return
-        dismissAll()
-        popup = XPopup.Builder(context).asCustom(PopupRadioGroup(context, title, radioList, checkedRadio, onSelectFinishListener))
-        popup?.show()
+        showPopup(XPopup.Builder(context).asCustom(PopupRadioGroup(context, title, radioList, checkedRadio, onSelectFinishListener)))
     }
 
     fun showMoodSelectPopup(withAll: Boolean, onMoodSelectListener: (mood: Int) -> Unit)
     {
         val context = ActivityUtils.getTopActivity() ?: return
-        dismissAll()
-        popup = XPopup.Builder(context).asCustom(PopupMoodSelect(context, withAll, onMoodSelectListener))
-        popup?.show()
+        showPopup(XPopup.Builder(context).asCustom(PopupMoodSelect(context, withAll, onMoodSelectListener)))
     }
 
     fun showInputPopup(title: String, defaultValue: String?, editHint: String, inputCompletedCallback: (String) -> Unit)
     {
         val context = ActivityUtils.getTopActivity() ?: return
-        dismissAll()
-        popup = XPopup.Builder(context).asCustom(PopupInput(context, title, defaultValue, editHint, inputCompletedCallback))
-        popup?.show()
+        showPopup(XPopup.Builder(context).asCustom(PopupInput(context, title, defaultValue, editHint, inputCompletedCallback)))
     }
 
     fun showDiaryTagListPopup(onTagSelectFinishListener: (String) -> Unit)
     {
         val context = ActivityUtils.getTopActivity() ?: return
-        dismissAll()
-        popup = XPopup.Builder(context).asCustom(PopupDiaryTagList(context, onTagSelectFinishListener))
-        popup?.show()
+        showPopup(XPopup.Builder(context).asCustom(PopupDiaryTagList(context, onTagSelectFinishListener)))
     }
 
     fun showDatePickerDialog(year: Int, month: Int, day: Int, dateSetListener: (View: DatePicker, Int, Int, Int) -> Unit)
@@ -126,17 +107,35 @@ object PopupUtil
     fun showImageViewerPopup(imageView: ImageView, showImage: String, imageList: List<String>)
     {
         val context = ActivityUtils.getTopActivity() ?: return
-        dismissAll()
-        popup = XPopup.Builder(context).asCustom(PopupImageViewer(context).apply {
+        showPopup(XPopup.Builder(context).asCustom(PopupImageViewer(context).apply {
             setSrcView(imageView, imageList.indexOf(showImage))
             setImageUrls(imageList.toMutableList())
-        })
-        popup?.show()
+        }))
     }
 
     fun dismissAll()
     {
-        popup?.dismiss()
-        dialog?.dismiss()
+        GlobalScope.launch(Dispatchers.Main) {
+            showPopupJob?.cancel()
+            curPopup?.dismiss()
+            dialog?.dismiss()
+        }
+    }
+
+    private fun showPopup(popup: BasePopupView)
+    {
+        GlobalScope.launch(Dispatchers.Main) {
+            dialog?.dismiss()
+            showPopupJob?.cancel()
+            showPopupJob = GlobalScope.launch(Dispatchers.Main) {
+                if (curPopup?.isShow == true)
+                {
+                    curPopup?.dismiss()
+                    delay(300)
+                }
+                curPopup = popup
+                curPopup?.show()
+            }
+        }
     }
 }
